f717e20b209e7fd8393c758f7c5050bb7c53bdf2
Raveen K Sharma <raveen08@gmail.com>
2020-05-08 23:30:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,7 @@ end
 
 name_array =  ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def batch_badge_creator(name_array)
+def batch_badge_creator(array)
   name_array.each do |nombre|
     "Hello, my name is #{nombre}."
   end
