341d9afb0191088137b9f3cbddb02158bbb168f7
Raveen K Sharma <raveen08@gmail.com>
2020-05-08 23:40:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,7 +17,7 @@ array1 = [1,2,3,4,5,6,7]
 
 def assign_rooms(array1)
 	array1.each_with_index.map do |name, index| 
-		"Hello #{name}! You'll be assigned to room #{index + 1}!"
+		"Hello, #{name}! You'll be assigned to room #{index + 1}!"
 	end
 end
 
