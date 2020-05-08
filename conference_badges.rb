edacba6baf14c942df06ac1163364ae98a7c7600
Raveen K Sharma <raveen08@gmail.com>
2020-05-08 23:31:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,3 +4,9 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
+guests = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def batch_badge_creator(array)
+  array.each do |name|
+    "Hello, my name is #{name}."
+  end
+end
